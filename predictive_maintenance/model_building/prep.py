818da68edbaa46832860d84040347c481b52f676
veerdevil <veerendra90.m@gmail.com>
2026-02-07 18:01:46 +0000

    Fixed file name by removing extra .csv

--- predictive_maintenance/model_building/prep.py
+++ predictive_maintenance/model_building/prep.py
@@ -12,7 +12,7 @@ from huggingface_hub import login, HfApi
 
 # Define constants for the dataset and output paths
 api = HfApi(token=os.getenv("HF_TOKEN"))
-DATASET_PATH = "hf://datasets/VeerendraManikonda/predictive-maintenance-engine-health-dataset/engine_data.csv.csv"
+DATASET_PATH = "hf://datasets/VeerendraManikonda/predictive-maintenance-engine-health-dataset/engine_data.csv"
 df = pd.read_csv(DATASET_PATH)
 print("Dataset loaded successfully.")
 
